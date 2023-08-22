f9770a2278bf89891e3e89705780b5ce81edfed8
Amith Prasad <amithprasad.246@gmail.com>
2023-08-23 00:55:33 +0530

    Alpaca Generator First Commit

---
+++ Alpaca/Assets.js
@@ -0,0 +1,80 @@
+const accessoriesMap={
+        earings: "../alpaca/accessories/earings.png",
+        flower: "../alpaca/accessories/flower.png",
+        glasses: "../alpaca/accessories/glasses.png",
+        headphone: "../alpaca/accessories/headphone.png",
+    };
+
+const backgroundsMap={
+        blue50: "../alpaca/backgrounds/blue50.png",
+        blue60: "../alpaca/backgrounds/blue60.png",
+        blue70: "../alpaca/backgrounds/blue70.png",
+        darkblue30: "../alpaca/backgrounds/darkblue30.png",
+        darkblue50: "../alpaca/backgrounds/darkblue50.png",
+        darkblue70: "../alpaca/backgrounds/darkblue70.png",
+        green50: "../alpaca/backgrounds/green50.png",
+        green60: "../alpaca/backgrounds/green60.png",
+        green70: "../alpaca/backgrounds/green60.png",
+        grey40: "../alpaca/backgrounds/grey40.png",
+        grey70: "../alpaca/backgrounds/grey70.png",
+        grey80: "../alpaca/backgrounds/grey80.png",
+        red50: "../alpaca/backgrounds/red50.png",
+        red60: "../alpaca/backgrounds/red60.png",
+        red70: "../alpaca/backgrounds/red70.png",
+        yellow50: "../alpaca/backgrounds/yellow50.png",
+        yellow60: "../alpaca/backgrounds/yellow60.png",
+        yellow70: "../alpaca/backgrounds/yellow70.png",
+    };
+
+const earsMap={
+        default: "../alpaca/ears/default.png",
+        tiltbackward: "../alpaca/ears/tilt-backward.png",
+        tiltforward: "../alpaca/ears/tilt-forward.png",
+    };
+
+const eyesMap={
+        default: "../alpaca/eyes/default.png",
+        angry: "../alpaca/eyes/angry.png",
+        naughty: "../alpaca/eyes/naughty.png",
+        panda: "../alpaca/eyes/panda.png",
+        smart: "../alpaca/eyes/smart.png",
+        star: "../alpaca/eyes/star.png",
+    };
+
+const hairMap={
+        default: "../alpaca/hair/default.png",
+        bang: "../alpaca/hair/bang.png",
+        curls: "../alpaca/hair/curls.png",
+        elegant: "../alpaca/hair/elegant.png",
+        fancy: "../alpaca/hair/fancy.png",
+        quiff: "../alpaca/hair/quiff.png",
+        short: "../alpaca/hair/short.png",
+    };
+
+const legMap={
+        default: "../alpaca/leg/default.png",
+        bubbletea: "../alpaca/leg/bubble-tea.png",
+        cookie: "../alpaca/leg/cookie.png",
+        gameconsole: "../alpaca/leg/game-console.png",
+        tiltbackward: "../alpaca/leg/tilt-backward.png",
+        tiltforward: "../alpaca/leg/tilt-forward.png",
+    };
+
+const mouthMap={
+        default: "../alpaca/mouth/default.png",
+        astonished: "../alpaca/mouth/astonished.png",
+        eating: "../alpaca/mouth/eating.png",
+        laugh: "../alpaca/mouth/laugh.png",
+        tongue: "../alpaca/mouth/tongue.png",
+    };
+const neckMap={
+        bendbackward: "../alpaca/neck/bend-backward.png",
+        bendforward: "../alpaca/neck/bend-forward.png",
+        default: "../alpaca/neck/default.png",
+        thick: "../alpaca/neck/thick.png",
+    };
+const noseMap={
+        nose: "../alpaca/nose.png"
+    };
+
+export {noseMap,neckMap,mouthMap,legMap,hairMap,eyesMap,earsMap,backgroundsMap,accessoriesMap};
